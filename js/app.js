5fbd4fe656265cbd5fc33ace5b7c1143270b6f00
lokesh2v <lokesh2.v@tcs.com>
2017-07-12 11:33:10 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3,9 +3,15 @@
  */
 
 var app = angular.module('gjapp', []);
-app.controller('glCtrl', function($scope, $http) {
-    $http.get("welcome.htm")
+
+app.controller('gjCtrl', function($scope, $http) {
+	
+	$scope.header = "Hello";
+	
+    $http.get("https://gjfunc.azurewebsites.net/api/gists?code=3k63ntwVVZVtr0dfXUksa0zYz9rYSRBayCccsIAgvW2yx5d1Tiq2cQ==")
     .then(function(response) {
-        $scope.myWelcome = response.data;
+        //$scope.myWelcome = response.data;
+    	alert(response.data);
     });
+    
 });
